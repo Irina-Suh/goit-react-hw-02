812cb66989120ed8438642c5f0f654e6254076be
Iryna <ikalchenko23@gmail.com>
2025-03-31 20:27:12 +0300

    update positiveFeedback and localstorege

--- src/App.jsx
+++ src/App.jsx
@@ -22,42 +22,56 @@ const App = () => {
       // setCounter(counter + 1);
         }
         const totalFeedback = click.good + click.neutral + click.bad;
-
+        const positiveFeedback = totalFeedback>0 ? (Math.round(click.good / totalFeedback * 100)) : 0;
+       
         const handleResetClick = () => {
          //  totalFeedback = 0;
          //  setCounter(0);
              setClick({
               good: 0,
-               neutral: 0,
+              neutral: 0,
               bad: 0
              });
           
           };
-          useEffect(() => {
-            const savedStats = JSON.parse(localStorage.getItem('feedback'));
-            if (savedStats) {
-                setClick(savedStats);
-            }
-        }, []);
 
-          useEffect(() => {
-            localStorage.setItem('feedback', JSON.stringify(click));
-        }, [click]);
+      useEffect(() => {
+        const saved = window.localStorage.getItem('feedback');
+        if (saved) {
+          const parsed = JSON.parse(saved);
+          setClick({
+              good: parsed.good,
+              neutral: parsed.neutral,
+              bad: parsed.bad})
+        }
+    }, []);
+
+
+        useEffect(() => {
+          const local = {
+              // click: click,
+              good: click.good,
+              neutral: click.neutral,
+              bad: click.bad,
+              totalFeedback: totalFeedback,
+              positiveFeedback: positiveFeedback
+              
+          };
+          localStorage.setItem('feedback', JSON.stringify(local));
+      }, [click, totalFeedback,positiveFeedback]);
 
         
  
   return (
     <>
 <Description/>
-<Options updateFeedback={updateFeedback}  totalFeedback={totalFeedback} handleResetClick ={handleResetClick}/>
+<Options updateFeedback={updateFeedback}  totalFeedback={totalFeedback} 
+         handleResetClick ={handleResetClick}/>
 
 { 
-(totalFeedback > 0) ? <Feedback click={click} totalFeedback={totalFeedback}/>//(<Notification message = {totalFeedback} />) 
+(totalFeedback > 0) ? <Feedback click={click} totalFeedback={totalFeedback}  positiveFeedback ={positiveFeedback} />//(<Notification message = {totalFeedback} />) 
                       : (<Notification message="No feedback yet" />)
 } 
-
-
-
 
     </>
   );
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import s from "./Feedback.module.css"
-const Feedback = ({click, totalFeedback}) => {
+const Feedback = ({click, totalFeedback,positiveFeedback}) => {
     return (
               <ul className={s.list}>
               <li>Good: {click.good}</li>
               <li>Neutral: {click.neutral}</li>
               <li>Bad: {click.bad}</li>
               <li>Total: {totalFeedback}</li>
-              <li>Positive: {Math.round(click.good / totalFeedback * 100)} %</li>
+              <li>Positive: {positiveFeedback}%</li>
               </ul>
          
       );
